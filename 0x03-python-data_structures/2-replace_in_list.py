776b71cb773aacc93eea41b45bece73d39a7deec
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-09 15:32:11 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
-	    my_list[i] = element
+            my_list[i] = element
     return my_list
