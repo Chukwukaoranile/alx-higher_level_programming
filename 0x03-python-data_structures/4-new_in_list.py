12fe7584b68005e9885dc99748c5a4b20c425dc2
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-09 15:28:52 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or len(my_list) - 1 < idx:
         return my_list
     new_list = list(my_list)
-    for i range(len(my_list)):
+    for i in range(len(my_list)):
         if idx != i:
             new_list[i] = my_list[i]
     else:
