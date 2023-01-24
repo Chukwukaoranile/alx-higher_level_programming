29a85de586b849b25303bae6655e06b60fe5cc1c
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-24 10:27:47 -0800

    Python class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -11,12 +11,12 @@ class Square:
 
     @property
     def size(self):
-        """Retrieves size."""
+        """Retrieves  size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets size."""
+        """Sets the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -24,17 +24,15 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns area of the square"""
-
+        """Returns the current square area"""
         return self.__size ** 2
 
-
-def my_print(self):
-    """Prints to the standard output"""
-    if self.__size == 0:
-        print("")
-    else:
-        for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
+    def my_print(self):
+        """Prints in stdout the square with the character #:"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
