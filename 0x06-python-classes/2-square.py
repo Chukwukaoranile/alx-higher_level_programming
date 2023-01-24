2e6470ba288f4cb3a6995b247b7bf72b1d34ae35
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-24 08:33:32 -0800

    Python Class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Define an object name Square.
-"""
+"""Define a square object."""
 
 
 class Square:
-    """ Object Square [class]
-    """
+    """Object Square [class]"""
     def __init__(self, size=0):
-        """ Initialize method.
+        """Initialization method.
         Args:
             self (class): This class
-            size (int): Size of the square
-        """
+            size (int): Size of the square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = int(size)
-Footer
-
