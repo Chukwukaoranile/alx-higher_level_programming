f3c3746f8fd04c3abd0fbee4f0c8535ddb5f3de7
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-24 04:21:59 -0800

    Error fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    a = 0
-    i = 0
-    while a < x:
+def safe_print_list_integers(my_list=[], x=0)
+var = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[a]), end="")
-            i = i + 1
-        except IndexError:
-            raise
-        except:
-            pass
-        a = a + 1
+            print("{:d}".format(my_list[i]), end="")
+            var += 1
+        except (ValueError, TypeError):
+            continue
     print("")
-    return i
+    return (var)
+Footer
