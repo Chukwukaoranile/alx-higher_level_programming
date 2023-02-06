ad396f0bf6949c52c6b4efdbc67a4a3eb8d1ed41
Achebe Chika Luke <prosperbinary@gmail.com>
2023-02-06 14:08:09 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''1-my_list Task'''
+
+
+class MyList(list):
+    '''Defining class MyList'''
+    def print_sorted(self):
+        '''A class MyList that inherits from list'''
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This folder contains files for ALX 0x0A-python-inheritance tasks
