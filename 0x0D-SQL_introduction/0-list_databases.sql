aeef7c66e5b43e762b46b3d4a7b6e8ee086ff7f4
chukwukaoranile <prosperbinary@gmail.com>
2023-03-14 12:00:01 -0700

    0-list_databases.sql

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- A script that lists all databases of your MySQL server
+-- A function that lists all database of MySQL
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+## This directory contains files for ALX 0x0D-SQL_introduction project
