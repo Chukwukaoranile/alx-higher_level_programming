09a162dedfd43760144e0eb5d73d2d19e7cfef59
chukwukaoranile <prosperbinary@gmail.com>
2023-03-15 06:27:07 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,15 +1,7 @@
 -- A script that creates a table called second_table and insers records in the current database in your MySQL server.
 -- Creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS Second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-INSERT INTO second_table (id, name, score)
-VALUES ('1', 'John', '10');
-INSERT INTO second_table (id, name, score)
-VALUES ('2', 'Alex', '3');
-INSERT INTO second_table (id, name, score)
-VALUES ('3', 'Bob', '14');
-INSERT INTO second_table (id, name, score)
-VALUES ('4', 'George', '8');
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table value('1', 'John', '10');
+INSERT INTO second_table value('2', 'Alex', '3');
+INSERT INTO second_table value('3', 'Bob', '14');
+INSERT INTO second_table value('4', 'George', '8');
