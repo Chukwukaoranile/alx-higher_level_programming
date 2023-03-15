0f5aa8050b0c8501248c04f91d522ab0c3a9a8f8
chukwukaoranile <prosperbinary@gmail.com>
2023-03-15 05:45:29 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- A script that inserts a new row in the table first_table
 -- To insert a row into a table.
-INSERT INTO hbtn_0c_0 (id, name)
+INSERT INTO first_table (id, name)
 VALUES (89, 'Best School');
