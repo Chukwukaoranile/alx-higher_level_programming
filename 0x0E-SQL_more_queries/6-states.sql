f7cebde9c159a66269e8751717a4eefb86d03aae
chukwukaoranile <prosperbinary@gmail.com>
2023-03-16 06:44:51 -0700

    15-groups.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- A script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- creates the database hbtn_0d_usa.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Create table with name state.
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id));
