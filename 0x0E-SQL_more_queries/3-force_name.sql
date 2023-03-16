082eb8fabd1d66153a114e3a36a6bd93d01ac70d
chukwukaoranile <prosperbinary@gmail.com>
2023-03-16 05:19:31 -0700

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- A script that creates the table force_name on your MySQL server.
+-- creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXIXTS hbtn_0d_2 ( id INT, name VARCHAR(256) NOT NULL);
