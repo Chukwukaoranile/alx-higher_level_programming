188c79d91f6e18c4859c04c776a6177ea223ac04
chukwukaoranile <prosperbinary@gmail.com>
2023-03-16 21:38:20 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,10 +2,6 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF EXISTS hbtn_0d_usa;
 USE hbtn_Od_usa;
-CREATE TABLE IF NOT EXISTS states (
-  id INT PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS cities (
   id INT NOT NULL AUTO_INCREMENT UNIQUE,
   state_id INT NOT NULL,
