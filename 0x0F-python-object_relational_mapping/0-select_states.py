52e48e90839a1f5341d6b20dd41774bf9f072d68
chukwukaoranile <prosperbinary@gmail.com>
2023-04-18 05:42:52 -0700

    ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    """
+    Connects to the MySQL database and queries data.
+    """
+    try:
+        db_connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8"
+        )
+    except MySQLdb.Error as error:
+        print("Can't connect to database: {}".format(error))
+        return
+    
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db_connection.close()
+
+
+if _name_ == '_main_':
+    mysqlconnect()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This directory contains files for ALX 0x0F-python-object_relational_mapping Project by Chika Achebe
