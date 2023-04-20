59e398ac81fb377025db7110e2f2371ef10e17ab
chukwukaoranile <prosperbinary@gmail.com>
2023-04-20 05:56:50 -0700

    ORM fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,29 +3,24 @@ import MySQLdb
 from sys import argv
 
 
-def mysqlconnect():
+def mysqlconnnect():
     """
-    Connects to the MySQL database and queries data.
+    Connwct and querry the database through the code base
     """
     try:
-        db_connection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset="utf8"
-        )
-    except MySQLdb.Error as error:
-        print("Can't connect to database: {}".format(error))
-        return
-    
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], passwd=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("can't connect to database")
+        return 0
+    cur = db_connection.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for data_row in query_rows:
+        print(data_row)
+    cur.close()
     db_connection.close()
 
-mysqlconnect()
+
+mysqlconnnect()
