08e078fc9982e54d8405417cb6b02bc64acc985e
chukwukaoranile <prosperbinary@gmail.com>
2023-04-20 06:43:02 -0700

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,19 +7,22 @@ def connectDB():
     """
     Connecting the database and query it through the code base
     """
-    if __name__ == '__main__':
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], password=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to database")
+        return 0
+    mem = db_connection.cursor()
+    sql = "SELECT id, name FROM states ORDER BY id ASC;"
+    mem.execute(sql)
+    query_rows = mem.fetchall()
+    for row in query_rows:
+        if 'N' == row[1][0]:
+            print(row)
+    mem.close()
+    db_connection.close()
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
-    me = db.cursor()
-    myQuery = " ".join([
-        "SELECT * FROM states",
-        "WHERE name LIKE BINARY 'N%'",
-        "ORDER BY id ASC"])
-    me.execute(myQuery)
-    res = me.fetchall()
-    for rows in res:
-        print(rows)
-    me.close()
-    db.close()
+
+connectDB()
