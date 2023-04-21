f92a502754da9d5994eff2b6ad52135baad5b38d
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 02:35:04 -0700

    ORM fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
+"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
 from sys import argv
 import MySQLdb
 
 
 def sqlConection():
     """
-    Conecting and quering to database
+    Connect and query the  database from the code base
     """
     try:
-        db_con = MySQLdb.connect(host="localhost", port=3306,
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
                                         user=argv[1], password=argv[2],
                                         db=argv[3], charset="utf8")
-      except Exception:
+    except Exception:
         print("Can't connect to database")
         return 0
-    cur = db_con.cursor()
+    cur = db_connection.cursor()
     sql = "SELECT c.id,c.name, s.name FROM cities AS c JOIN states AS s "
-    string = "WHERE c.state_id=s.id ORDER BY id ASC"
-    sqlc = sql + string
+    strin = "WHERE c.state_id=s.id ORDER BY id ASC"
+    sqlc = sql + strin
     cur.execute(sqlc)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    db_con.close()
+    db_connection.close()
 
 
 sqlConection()
