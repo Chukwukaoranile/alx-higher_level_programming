c6ae4ff7ee31a204329ff6430fdeb14be8aff95d
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 00:53:22 -0700

    ORM fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db_connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db_connection.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
