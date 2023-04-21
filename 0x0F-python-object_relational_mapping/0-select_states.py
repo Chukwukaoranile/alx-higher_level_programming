869f70c5bc7b39a3b2d912ab2ee2ed2bcba5698e
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 01:00:06 -0700

    ORM fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-'''Write a script that lists all states from the database hbtn_0e_0_usa: '''
 import MySQLdb
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    db_connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db_connection.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
+def mysqlconnnect():
+    """
+    Connect and query data base from code base
+    """
+    try:
+        db_con = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], passwd=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("can't connect to database")
+        return 0
+    cur = db_con.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db_connection.close()
+    db_con.close()
+
+
+mysqlconnnect()
