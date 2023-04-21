a0ed3745866c5384285bc432c868ea1ff197e0c5
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 02:27:45 -0700

    ORM fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,25 +2,25 @@
 from sys import argv
 import MySQLdb
 
-if __name__ == '__main__':
-    def sqlConection():
-        """
-        Conecting and quering to database
-        """
-        try:
-            db_con = MySQLdb.connect(host="localhost", port=3306,
-                                            user=argv[1], password=argv[2],
-                                            db=argv[3], charset="utf8")
-        except Exception:
-            print("Can't connect to database")
-            return 0
-        cur = db_con.cursor()
-        sql = "SELECT c.id,c.name, s.name FROM cities AS c JOIN states AS s "
-        string = "WHERE c.state_id=s.id ORDER BY id ASC"
-        sqlc = sql + string
-        cur.execute(sqlc)
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        db_con.close()
+
+def sqlConection():
+    """
+    Conecting and quering to database
+    """
+    try:
+        db_con = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], password=argv[2],
+                                        db=argv[3], charset="utf8")
+      except Exception:
+        print("Can't connect to database")
+        return 0
+    cur = db_con.cursor()
+    sql = "SELECT c.id,c.name, s.name FROM cities AS c JOIN states AS s "
+    string = "WHERE c.state_id=s.id ORDER BY id ASC"
+    sqlc = sql + string
+    cur.execute(sqlc)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_con.close()
