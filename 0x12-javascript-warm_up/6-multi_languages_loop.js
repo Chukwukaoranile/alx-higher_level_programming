03a5719681e3038cb799b65c5426d1a71d7b85d8
chukwukaoranile <prosperbinary@gmail.com>
2023-04-24 11:50:50 +0100

    JavaScript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < message.length; i++) {
-	  console.log(message[i]);
+const arrS = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arrS.length; i++) {
+  console.log(arrS[i]);
 }
