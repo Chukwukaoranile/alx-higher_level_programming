79d00bf87becef80fc69b001229a75b1f763c1ad
chukwukaoranile <prosperbinary@gmail.com>
2023-05-01 16:25:29 +0000

    0x10-python-network_0

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,12 +2,11 @@
 """A Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
 
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     import requests
-
-    igede = requests.get(argv[1])
-
-    if igede.status_code >= 400:
-        print(igede.text)
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print("Error code", igede.status_code)
+        print(r.text)
