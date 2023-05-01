b6c801b436fe972638cc18929f9f806781b2bb53
chukwukaoranile <prosperbinary@gmail.com>
2023-05-01 15:28:50 +0000

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-""" 
-A function that finds a peak in a list of unsorted integers.
- """
-
+""" A function that finds a peak in a list of unsorted integers. """
 
 def find_peak(list_of_integers):
     if list_of_integers == []:
