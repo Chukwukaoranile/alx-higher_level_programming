a4608e0259710c06e9c862b89183710e3897488d
chukwukaoranile <prosperbinary@gmail.com>
2023-05-25 00:02:47 +0100

    Web

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,30 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
+const url = 'http://swapi.co/api/films/';
+let mj = parseInt(process.argv[2], 10);
+let characters = [];
 
-const getChar = async (url) => {
-  const name = await axios.get(url);
-  return (name.data.name);
-};
-
-const getAll = async (url) => {
-  const response = await axios.get(url);
-  for (const character of response.data.characters) {
-    const newCaracter = await getChar(character);
-    console.log(newCaracter);
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    if (mj < 4) {
+      mj += 3;
+    } else {
+      mj -= 3;
+    }
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].episode_mj === mj) {
+        characters = results[i].characters;
+        break;
+      }
+    }
+    for (let j = 0; j < characters.length; j++) {
+      request(characters[j], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-};
-
-getAll(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`);
+});
